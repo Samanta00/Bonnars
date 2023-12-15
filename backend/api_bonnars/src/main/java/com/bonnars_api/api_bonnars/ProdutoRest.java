e447e14250a922bdc03fc9f2f252dc2afb87e831
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-15 19:27:16 -0300

    feat: delegacao da criação de objetos

--- backend/api_bonnars/src/main/java/com/bonnars_api/api_bonnars/ProdutoRest.java
+++ backend/api_bonnars/src/main/java/com/bonnars_api/api_bonnars/ProdutoRest.java
@@ -1,5 +1,10 @@
 package com.bonnars_api.api_bonnars;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bonnars_api.api_bonnars.database.RepositorioProdutos;
+
 public class ProdutoRest {
-    
+    @Autowired
+    private RepositorioProdutos produto;
 }
