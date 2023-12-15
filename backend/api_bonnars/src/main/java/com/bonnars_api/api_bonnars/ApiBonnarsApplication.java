020614cba1f822218a041820801d74905420ac5f
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-15 18:17:47 -0300

    fear: criação do backend

---
+++ backend/api_bonnars/src/main/java/com/bonnars_api/api_bonnars/ApiBonnarsApplication.java
@@ -0,0 +1,13 @@
+package com.bonnars_api.api_bonnars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBonnarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBonnarsApplication.class, args);
+	}
+
+}
---
+++ backend/api_bonnars/src/test/java/com/bonnars_api/api_bonnars/ApiBonnarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonnars_api.api_bonnars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBonnarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
