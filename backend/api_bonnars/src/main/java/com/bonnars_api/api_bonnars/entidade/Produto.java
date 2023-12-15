4c90ae502838ea3e9c3aa49ca97b9c2b648b10c9
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-15 18:59:17 -0300

    feat: definição de tipo de funções

--- backend/api_bonnars/src/main/java/com/bonnars_api/api_bonnars/entidade/Produto.java
+++ backend/api_bonnars/src/main/java/com/bonnars_api/api_bonnars/entidade/Produto.java
@@ -1,5 +1,61 @@
 package com.bonnars_api.api_bonnars.entidade;
 
+import java.time.LocalDate;
+
+
+
+
 public class Produto {
-    
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public LocalDate getValidade() {
+        return validade;
+    }
+    public void setValidade(LocalDate validade) {
+        this.validade = validade;
+    }
+    public String getGeneroProduto() {
+        return generoProduto;
+    }
+    public void setGeneroProduto(String generoProduto) {
+        this.generoProduto = generoProduto;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    private Long id;
+    private String nome;
+    private double preco;
+    private String descricao;
+    private LocalDate validade;
+    private String generoProduto;
+    private String marca;
+
+
 }
