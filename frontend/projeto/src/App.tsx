add0be2077d712ee73ec0726b600d03cb899a13a
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-16 12:53:03 -0300

    fix: chamado de rotas no app

--- frontend/projeto/src/App.tsx
+++ frontend/projeto/src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import './App.css'
-import Cards from './components/Cards';
-import MaisProcurados from './components/MaisProcurados';
-import Footer from './components/Footer';
-import NavBar from './components/NavBar'
-import Slide from './components/Slide'
+import AppRouter from './routes/PublicRouters';
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Slide />
-      <Cards />
-      <MaisProcurados />
-      <Footer />
+      <AppRouter/>
     </div>
   )
 }
