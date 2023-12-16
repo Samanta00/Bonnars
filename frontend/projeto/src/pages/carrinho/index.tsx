bfd57bf67b7c4dbaf8281e4488ff21f39708e01b
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-16 14:59:06 -0300

    fix: correção e adição de states ao carrinho

--- frontend/projeto/src/pages/carrinho/index.tsx
+++ frontend/projeto/src/pages/carrinho/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from 'react';
+
 import './carrinho.css'
 
 interface CartItem {
@@ -9,29 +10,44 @@ interface CartItem {
 }
 
 export default function Carrinho() {
-    const cartItems: CartItem[] = [
+    const [cartItems, setCartItems] = useState<CartItem[]>([
         { id: 1, name: 'Item 1', price: 10, quantity: 2 },
         { id: 2, name: 'Item 2', price: 15, quantity: 1 },
         { id: 3, name: 'Item 3', price: 20, quantity: 3 },
-      ];
+      ]);
     
       const getTotalPrice = () => {
         return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
       };
+    
+      const handleCancel = (id: number) => {
+        const updatedCart = cartItems.filter((item) => item.id !== id);
+        setCartItems(updatedCart);
+      };
 
     return (
         <div className="cart-container">
-            <h2>Shopping Cart</h2>
-            <div className="cart-items">
-                {cartItems.map((item) => (
-                    <div key={item.id} className="cart-item">
-                        <span>{item.name}</span>
-                        <span>Quantity: {item.quantity}</span>
-                        <span>Price: ${item.price * item.quantity}</span>
-                    </div>
-                ))}
+        <h2>Shopping Cart</h2>
+        <div className="cart-items">
+          {cartItems.map((item) => (
+            <div key={item.id} className="cart-item">
+              <div className="item-details">
+                <span>{item.name}</span>
+                <span>Quantity: {item.quantity}</span>
+                <span>Price: ${item.price * item.quantity}</span>
+              </div>
+              <button className="cancel-button" onClick={() => handleCancel(item.id)}>
+                X
+              </button>
             </div>
-            <div className="total">Total: ${getTotalPrice()}</div>
+          ))}
         </div>
+        {cartItems.length > 0 && (
+          <div className="cart-footer">
+            <div className="total">Total: ${getTotalPrice()}</div>
+            <button className="checkout-button">Finalizar Compra</button>
+          </div>
+        )}
+      </div>
     );
 }
