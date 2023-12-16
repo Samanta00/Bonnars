1be6eb1314fd2dd85e263982b853e8b0b0a6b07f
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-16 16:54:48 -0300

    lint: organização e limpeza de codigo

--- frontend/projeto/src/pages/catalogo/index.tsx
+++ frontend/projeto/src/pages/catalogo/index.tsx
@@ -1,58 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Filtro from './Filtro';
 
-interface ClothingItem {
-  id: number;
-  name: string;
-  category: string;
-  size: string;
-  color: string;
-  material: string;
-  price: number;
-  image: string;
-}
+const Catalogo = () => {
 
-interface CatalogProps {
-    clothingItems: ClothingItem[];
-}
+  const clothingItems = [
+    {
+      id: 1,
+      name: 'Camisa',
+      category: 'Masculino',
+      size: 'M',
+      color: 'Azul',
+      material: 'Algodão',
+      price: 25.99,
+      image: 'caminho/para/imagem.jpg',
+    },
+    
+  ];
 
-const Catalogo=()=>{
-    const [filteredItems, setFilteredItems] = useState<ClothingItem[]>([]);
-    const clothingItems: ClothingItem[] = [
-        {
-          id: 1,
-          name: 'Camiseta',
-          category: 'Roupas',
-          size: 'M',
-          color: 'Azul',
-          material: 'Algodão',
-          price: 25.99,
-          image: 'caminho/para/imagem.jpg',
-        },
-     
-      ];
-  const applyFilters = (filteredData: ClothingItem[]) => {
-    setFilteredItems(filteredData);
-  };
-    return(
-        <div className="filter-panel">
-        <h1>Catálogo</h1>
-        <Filtro clothingItems={clothingItems} onFilterChange={applyFilters} />
-       
-        <div className="clothing-items">
-          {filteredItems.map((item) => (
-            <div key={item.id} className="clothing-item">
-              <img src={item.image} alt={item.name} />
-              <h3>{item.name}</h3>
-              <p>Tamanho: {item.size}</p>
-              <p>Cor: {item.color}</p>
-              <p>Material: {item.material}</p>
-              <p>Preço: ${item.price}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    )
-}
+  return (
+    <div>
+      <h1>Seu Catálogo</h1>
+      
+      <Filtro clothingItems={clothingItems} />
+    </div>
+  );
+};
 
 export default Catalogo;
