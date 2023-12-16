6610db2ae936154bf85fbeffd52ea5725192eafe
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-16 15:43:46 -0300

    feat: adicionado rotas de carrinho e catalogo

--- frontend/projeto/src/routes/PublicRouters/index.tsx
+++ frontend/projeto/src/routes/PublicRouters/index.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../../pages/home';
 import PaginaPadrao from '../../components/PaginaPadrao';
+import Carrinho from '../../pages/carrinho';
+import Catalogo from '../../pages/catalogo';
 
 export default function AppRouter() {
   return (
@@ -9,6 +11,8 @@ export default function AppRouter() {
     
         <Route path="/" element={<PaginaPadrao/>} >
             <Route index element={<Home/>}/> 
+            <Route path='carrinho' index element={<Carrinho/>}/>
+            <Route path='catalogo' index element={<Catalogo/>}/>
         </Route>
     
       </Routes>
