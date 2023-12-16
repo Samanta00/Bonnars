ebcd6bf68fe9db3aac0e4216f3735d1140e11b94
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-16 16:54:27 -0300

    lint: organização e limpeza de código

--- frontend/projeto/src/pages/catalogo/Filtro/index.tsx
+++ frontend/projeto/src/pages/catalogo/Filtro/index.tsx
@@ -1,92 +1,39 @@
-import React from 'react';
-
-interface ClothingItem {
-  id: number;
-  name: string;
-  category: string;
-  size: string;
-  color: string;
-  material: string;
-  price: number;
-  image: string;
-}
+import React, { useState } from 'react';
+import LogicaFiltro from './logica'; 
+import ClothingCard, { ClothingItem } from './ClothingCard'; // Importe a interface ClothingItem corretamente
 
 interface FiltroProps {
   clothingItems: ClothingItem[];
-  onFilterChange: (filters: any) => void;
 }
 
-const Filtro: React.FC<FiltroProps> = ({ clothingItems, onFilterChange }) => {
-    const getUniqueValues = <K extends keyof ClothingItem>(key: K): ClothingItem[K][] => {
-        return Array.from(new Set(clothingItems.map((item) => item[key])));
-    };
-      
+const Filtro: React.FC<FiltroProps> = ({ clothingItems }) => {
+  const [filteredItems, setFilteredItems] = useState<ClothingItem[]>(clothingItems);
 
-  const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const { name, value } = event.target;
-    onFilterChange({ [name]: value });
+  type FilterKeys = 'size' | 'color' | 'material' | 'price'; // Defina as chaves que podem ser filtradas
+
+  const handleFilterChange = (filters: Partial<Record<FilterKeys, string>>) => {
+    const filtered = clothingItems.filter((item) => {
+      for (const key in filters) {
+        if (Object.prototype.hasOwnProperty.call(filters, key)) {
+          const filterValue = filters[key as FilterKeys]; // Diga ao TypeScript que a chave é uma das chaves permitidas
+          if (filterValue && String(item[key as FilterKeys]) !== filterValue) {
+            return false;
+          }
+        }
+      }
+      return true;
+    });
+    setFilteredItems(filtered);
   };
-
-  const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    onFilterChange({ [name]: value });
-  };
-
-  const sizes = getUniqueValues('size');
-  const colors = getUniqueValues('color');
-  const materials = getUniqueValues('material');
-  const priceRange = {
-    min: Math.min(...clothingItems.map((item) => item.price)),
-    max: Math.max(...clothingItems.map((item) => item.price)),
-  };
-
+  
   return (
-    <div className="filter-panel">
-      <h3>Filters</h3>
-      <div className="filter-section">
-        <label>Tamanho:</label>
-        <select name="size" onChange={handleFilterChange}>
-          <option value="">All</option>
-          {sizes.map((size) => (
-            <option key={size} value={size}>
-              {size}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="filter-section">
-        <label>Cor:</label>
-        <select name="color" onChange={handleFilterChange}>
-          <option value="">All</option>
-          {colors.map((color) => (
-            <option key={color} value={color}>
-              {color}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="filter-section">
-        <label>Material:</label>
-        <select name="material" onChange={handleFilterChange}>
-          <option value="">All</option>
-          {materials.map((material) => (
-            <option key={material} value={material}>
-              {material}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="filter-section">
-        <label>Faixa de Preço:</label>
-        <input
-          type="range"
-          min={priceRange.min}
-          max={priceRange.max}
-          step={10}
-          name="price"
-          onChange={handlePriceChange}
-        />
-        <span>${priceRange.min} - ${priceRange.max}</span>
+    <div className="catalog">
+      <LogicaFiltro clothingItems={clothingItems} onFilterChange={handleFilterChange} />
+      <div className="catalog-items">
+        {/* Renderize os itens filtrados aqui */}
+        {filteredItems.map((item) => (
+          <ClothingCard key={item.id} item={item} />
+        ))}
       </div>
     </div>
   );
