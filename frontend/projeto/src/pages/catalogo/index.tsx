e775b3a16bfce512215542b51cb7dee0e1716218
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-16 18:44:18 -0300

    fix: adicionando mais objetos para teste

--- frontend/projeto/src/pages/catalogo/index.tsx
+++ frontend/projeto/src/pages/catalogo/index.tsx
@@ -1,29 +1,128 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Filtro from './Filtro';
+import ClothingCard from './Filtro/ClothingCard';
+
+interface ClothingItem {
+  id: number;
+  name: string;
+  category: string;
+  size: string;
+  color: string;
+  material: string;
+  price: number;
+  image: string;
+}
 
 const Catalogo = () => {
-
-  const clothingItems = [
+  const [filteredItems, setFilteredItems] = useState<ClothingItem[]>([]);
+  const [clothingItems] = useState<ClothingItem[]>([
     {
       id: 1,
-      name: 'Camisa',
+      name: 'Camisa Azul',
       category: 'Masculino',
       size: 'M',
       color: 'Azul',
       material: 'Algodão',
       price: 25.99,
-      image: 'caminho/para/imagem.jpg',
+      image: 'caminho/para/imagem1.jpg',
     },
-    
-  ];
+    {
+      id: 2,
+      name: 'Vestido Floral',
+      category: 'Feminino',
+      size: 'S',
+      color: 'Rosa',
+      material: 'Algodão',
+      price: 39.99,
+      image: 'caminho/para/imagem2.jpg',
+    },
+    {
+      id: 3,
+      name: 'Camisa Azul',
+      category: 'Masculino',
+      size: 'M',
+      color: 'Azul',
+      material: 'Algodão',
+      price: 25.99,
+      image: 'caminho/para/imagem1.jpg',
+    },
+    {
+      id: 4,
+      name: 'Vestido Floral',
+      category: 'Feminino',
+      size: 'S',
+      color: 'Rosa',
+      material: 'Algodão',
+      price: 39.99,
+      image: 'caminho/para/imagem2.jpg',
+    },
+    {
+      id: 5,
+      name: 'Camisa Azul',
+      category: 'Masculino',
+      size: 'M',
+      color: 'Azul',
+      material: 'Algodão',
+      price: 25.99,
+      image: 'caminho/para/imagem1.jpg',
+    },
+    {
+      id: 6,
+      name: 'Vestido Floral',
+      category: 'Feminino',
+      size: 'S',
+      color: 'Rosa',
+      material: 'Algodão',
+      price: 39.99,
+      image: 'caminho/para/imagem2.jpg',
+    },
+    {
+      id: 7,
+      name: 'Camisa Azul',
+      category: 'Masculino',
+      size: 'M',
+      color: 'Azul',
+      material: 'Algodão',
+      price: 25.99,
+      image: 'caminho/para/imagem1.jpg',
+    },
+    {
+      id: 8,
+      name: 'Vestido Floral',
+      category: 'Feminino',
+      size: 'S',
+      color: 'Rosa',
+      material: 'Algodão',
+      price: 39.99,
+      image: 'caminho/para/imagem2.jpg',
+    },
+  ]);
+
+  const handleFilteredItems = (filteredItems: ClothingItem[]) => {
+    setFilteredItems(filteredItems);
+  };
 
   return (
     <div>
       <h1>Seu Catálogo</h1>
-      
-      <Filtro clothingItems={clothingItems} />
+      <div style={{ display: 'flex' }}>
+        <div style={{ flex: 1 }}>
+          <Filtro clothingItems={clothingItems} onFilteredItems={handleFilteredItems} />
+        </div>
+        <div style={{ flex: 2 }}>
+          <div className="catalog-items">
+            {(filteredItems.length > 0 ? filteredItems : clothingItems).map((item) => (
+              <div key={item.id}>
+                <p>{item.name}</p>
+                <ClothingCard item={item} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
+
 
 export default Catalogo;
