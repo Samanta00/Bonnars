c8816402bdab56c74d836093039b4123c9783168
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-16 12:52:01 -0300

    fix: ajustes de organização da página home

---
+++ frontend/projeto/src/pages/home/index.tsx
@@ -0,0 +1,18 @@
+import Cards from "../../components/Cards";
+import Slide from "../../components/Slide";
+import MaisProcurados from "../MaisProcurados";
+
+
+
+function Home() {
+    return (
+      <div className="App">
+    
+        <Slide />
+        <Cards />
+        <MaisProcurados />
+      
+      </div>
+    )
+  }
+export default Home;
