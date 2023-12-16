fbdbc315bdff36feb87e2b88b0f2f84aaa470585
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-16 10:21:48 -0300

    feat: adição do componente MaisProcurados em app.tsx

--- frontend/projeto/src/App.tsx
+++ frontend/projeto/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css'
-import Cards from './components/Cards'
+import Cards from './components/Cards';
+import MaisProcurados from './components/MaisProcurados';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar'
+import Slide from './components/Slide'
 
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Slide />
       <Cards />
+      <MaisProcurados />
       <Footer />
     </div>
   )
