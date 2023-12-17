82d195e049c839a01dc37137c3250c7aa20a338c
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-17 17:56:47 -0300

    feat: adicionando api para deletar produto do carrinho

--- frontend/projeto/src/pages/carrinho/index.tsx
+++ frontend/projeto/src/pages/carrinho/index.tsx
@@ -1,53 +1,72 @@
-import React, { useState } from 'react';
-
-import './carrinho.css'
+import React, { useState, useEffect } from 'react';
+import './Styles/carrinho.css';
 
 interface CartItem {
-    id: number;
-    name: string;
-    price: number;
-    quantity: number;
+  id: number;
+  nome: string;
+  preco: number;
+  quantidade: number;
 }
 
 export default function Carrinho() {
-    const [cartItems, setCartItems] = useState<CartItem[]>([
-        { id: 1, name: 'Item 1', price: 10, quantity: 2 },
-        { id: 2, name: 'Item 2', price: 15, quantity: 1 },
-        { id: 3, name: 'Item 3', price: 20, quantity: 3 },
-      ]);
-    
-      const getTotalPrice = () => {
-        return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
-      };
-    
-      const handleCancel = (id: number) => {
-        const updatedCart = cartItems.filter((item) => item.id !== id);
-        setCartItems(updatedCart);
-      };
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
-    return (
-        <div className="cart-container">
-        <h2>Shopping Cart</h2>
-        <div className="cart-items">
-          {cartItems.map((item) => (
-            <div key={item.id} className="cart-item">
-              <div className="item-details">
-                <span>{item.name}</span>
-                <span>Quantity: {item.quantity}</span>
-                <span>Price: ${item.price * item.quantity}</span>
-              </div>
-              <button className="cancel-button" onClick={() => handleCancel(item.id)}>
-                X
-              </button>
+  useEffect(() => {
+    fetch('http://localhost:8080/carrinho')
+      .then((response) => response.json())
+      .then((data) => {
+        setCartItems(data);
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar os dados:', error);
+      });
+  }, []);
+
+  const getTotalPrice = () => {
+    return cartItems.reduce((total, item) => total + item.preco * item.quantidade, 0);
+  };
+
+  const handleCancel = (id: number) => {
+    fetch(`http://localhost:8080/carrinho/${id}`, {
+      method: 'DELETE'
+    })
+      .then((response) => {
+        if (response.ok) {
+          const updatedCart = cartItems.filter((item) => item.id !== id);
+          setCartItems(updatedCart);
+        } else {
+          console.error('Erro ao deletar item do carrinho');
+        }
+      })
+      .catch((error) => {
+        console.error('Erro ao fazer a requisição DELETE:', error);
+      });
+  };
+  
+
+  return (
+    <div className="cart-container">
+      <h2>Shopping Cart</h2>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <div key={item.id} className="cart-item">
+            <div className="item-details">
+              <span>{item.nome}</span>
+              <span>Quantity: {item.quantidade}</span>
+              <span>Price: ${item.preco * item.quantidade}</span>
             </div>
-          ))}
-        </div>
-        {cartItems.length > 0 && (
-          <div className="cart-footer">
-            <div className="total">Total: ${getTotalPrice()}</div>
-            <button className="checkout-button">Finalizar Compra</button>
+            <button className="cancel-button" onClick={() => handleCancel(item.id)}>
+              X
+            </button>
           </div>
-        )}
+        ))}
       </div>
-    );
+      {cartItems.length > 0 && (
+        <div className="cart-footer">
+          <div className="total">Total: ${getTotalPrice()}</div>
+          <button className="checkout-button">Finalizar Compra</button>
+        </div>
+      )}
+    </div>
+  );
 }
