5bb084662a9c881273125cff4f4562d53abfc508
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-17 10:13:39 -0300

    feat: adicionando cors na api

--- backend/api_bonnars/src/main/java/com/bonnars_api/api_bonnars/CarrinhoRest.java
+++ backend/api_bonnars/src/main/java/com/bonnars_api/api_bonnars/CarrinhoRest.java
@@ -2,6 +2,7 @@ package com.bonnars_api.api_bonnars;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import java.util.List;
 import com.bonnars_api.api_bonnars.entidade.Carrinho;
 
 @RestController
+@CrossOrigin(origins="*")
 @RequestMapping("/carrinho")
 public class CarrinhoRest {
     @Autowired
