c511725c5c328657ac419abe81f72c7bf5a1b8e1
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-17 20:10:30 -0300

    fix: adição de todos os objetos para a interface de CartIem

--- frontend/projeto/src/pages/carrinho/index.tsx
+++ frontend/projeto/src/pages/carrinho/index.tsx
@@ -1,16 +1,22 @@
+// Carrinho.tsx
 import React, { useState, useEffect } from 'react';
 import './Styles/carrinho.css';
+import Modal from './modal';
 
 interface CartItem {
   id: number;
   nome: string;
   preco: number;
   quantidade: number;
+  cor: string;
+  tamanho: string;
+  imagem: string;
 }
 
 export default function Carrinho() {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
   const [showModal, setShowModal] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<CartItem | null>(null);
 
   useEffect(() => {
     fetch('http://localhost:8080/carrinho')
@@ -28,15 +34,28 @@ export default function Carrinho() {
   };
 
   const handleEdit = (id: number) => {
-    console.log(id)
-    setShowModal(true); 
+    fetch(`http://localhost:8080/carrinho/${id}`, {
+      method: 'GET'
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Erro ao buscar informações do carrinho');
+        }
+      })
+      .then((data) => {
+        setShowModal(true);
+        setSelectedProduct(data);
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar informações do carrinho:', error);
+      });
   };
 
   const handleCloseModal = () => {
-    setShowModal(false); 
+    setShowModal(false);
   };
-
-
 
   const handleCancel = (id: number) => {
     fetch(`http://localhost:8080/carrinho/${id}`, {
@@ -54,7 +73,6 @@ export default function Carrinho() {
         console.error('Erro ao fazer a requisição DELETE:', error);
       });
   };
-  
 
   return (
     <div className="cart-container">
@@ -67,19 +85,16 @@ export default function Carrinho() {
               <span>Quantity: {item.quantidade}</span>
               <span>Price: ${item.preco * item.quantidade}</span>
             </div>
-            
             <div className='alinhamentoBotoes'>
-                  <i
-                    className="bi bi-pencil-fill fs-3 me-2"
-                    onClick={() => handleEdit(item.id)}
-                    id='iconeEditar'
-                  ></i>
-                <button className="cancel-button" onClick={() => handleCancel(item.id)}>
-                  X
-                </button>
-                
+              <i
+                className="bi bi-pencil-fill fs-3 me-2"
+                onClick={() => handleEdit(item.id)}
+                id='iconeEditar'
+              ></i>
+              <button className="cancel-button" onClick={() => handleCancel(item.id)}>
+                X
+              </button>
             </div>
-
           </div>
         ))}
       </div>
@@ -90,15 +105,8 @@ export default function Carrinho() {
         </div>
       )}
 
-      {showModal && (
-        <div className="modal">
-          <div className="modal-content">
-            {/* Conteúdo do modal */}
-            <h3>Editar Produto</h3>
-            {/* Inclua os campos de edição e botões de salvar/cancelar */}
-            <button onClick={handleCloseModal}>Fechar</button>
-          </div>
-        </div>
+      {showModal && selectedProduct && (
+        <Modal item={selectedProduct} handleCloseModal={handleCloseModal} />
       )}
 
     </div>
