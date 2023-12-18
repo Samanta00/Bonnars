b198efe51331f840f6cfb1c2fbd78aa058f27d15
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-18 18:09:26 -0300

    fix: troca de imagens ao slide

--- front/projeto/src/components/Slide/index.tsx
+++ front/projeto/src/components/Slide/index.tsx
@@ -1,15 +1,31 @@
 import React from "react";
-import slideMobile1 from "../../assets/Mobile/banner1-mobile.png";
-import slideTablet1 from "../../assets/Tablet/banner1-tablet.png";
-import slideDesktop1 from "../../assets/Desktop/banner1-desktop.png";
-import slideMobile2 from "../../assets/Mobile/banner2-mobile.png";
-import slideTablet2 from "../../assets/Tablet/banner2-tablet.png";
-import slideDesktop2 from "../../assets/Desktop/banner2-desktop.png";
-import slideMobile3 from "../../assets/Mobile/banner3-mobile.png";
-import slideTablet3 from "../../assets/Tablet/banner3-tablet.png";
-import slideDesktop3 from "../../assets/Desktop/banner3-desktop.png";
+import slideMobile1 from "../../assets/Mobile/1.png";
+import slideTablet1 from "../../assets/Tablet/1.png";
+import slideDesktop1 from "../../assets/Desktop/1.png";
+
+import slideMobile2 from "../../assets/Mobile/2.png";
+import slideTablet2 from "../../assets/Tablet/2.png";
+import slideDesktop2 from "../../assets/Desktop/2.png";
+
+import slideMobile3 from "../../assets/Mobile/3.png";
+import slideTablet3 from "../../assets/Tablet/3.png";
+import slideDesktop3 from "../../assets/Desktop/3.png";
+
+
 
 export default function Slide() {
+ 
+  const style = {
+    "font-size": "1.25rem",
+    "color": "white",
+    "font-weight":"bold"
+  };
+
+  const styleParagrafo = {
+    "font-size": "1.25rem",
+    "color": "white"
+  };
+
   return (
     <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
       <div className="carousel-indicators">
@@ -20,20 +36,20 @@ export default function Slide() {
       <div className="carousel-inner">
         <div className="carousel-item active">
         <div className="carousel-caption d-none d-md-block">
-            <h5>COLEÇÃO ATEMPORAL</h5>
-            <p>Estilo e qualidade para durar.</p>
+            <h5 style={style}>COLEÇÃO ATEMPORAL</h5>
+            <p style={styleParagrafo}>Estilo e qualidade para durar.</p>
           </div>
           <img 
-            className="img-fluid d-md-none" src={slideMobile3} 
+            className="img-fluid d-md-none" src={slideMobile1} 
             alt="Modelo feminina vestindo blusa violeta" 
           />
           <img 
-            className="img-fluid d-none d-md-block d-xl-none" src={slideTablet3} 
+            className="img-fluid d-none d-md-block d-xl-none" src={slideTablet1} 
             alt="Modelo feminina vestindo blusa violeta" 
           />
           <img 
-            className="img-fluid d-none d-xl-block" src={slideDesktop3} 
-            alt="Modelo feminina vestindo blusa violeta" 
+            className="img-fluid d-none d-xl-block" src={slideDesktop1} 
+            alt="Modelo feminina vestindo blusa violeta"
           />
         </div>
         <div className="carousel-item">
@@ -50,26 +66,26 @@ export default function Slide() {
             alt="Modelo feminina vestindo blusa violeta" 
           />
           <div className="carousel-caption d-none d-md-block">
-            <h5>COLEÇÃO ATEMPORAL</h5>
-            <p>Estilo e qualidade para durar.</p>
+            <h5 style={style}>COLEÇÃO ATEMPORAL</h5>
+            <p style={styleParagrafo}>Estilo e qualidade para durar.</p>
           </div>
         </div>
         <div className="carousel-item">
           <img 
             className="img-fluid d-md-none" src={slideMobile3} 
-            alt="Modelo feminina vestindo blusa violeta" 
+            alt="Modelo feminina vestindo blusa violeta"
           />
           <img 
             className="img-fluid d-none d-md-block d-xl-none" src={slideTablet3} 
-            alt="Modelo feminina vestindo blusa violeta" 
+            alt="Modelo feminina vestindo blusa violeta"
           />
           <img 
             className="img-fluid d-none d-xl-block" src={slideDesktop3} 
-            alt="Modelo feminina vestindo blusa violeta" 
+            alt="Modelo feminina vestindo blusa violeta"
           />
           <div className="carousel-caption d-none d-md-block">
-            <h5>COLEÇÃO ATEMPORAL</h5>
-            <p>Alto impacto visual, baixo impacto ambiental!</p>
+            <h5 style={style}>COLEÇÃO ATEMPORAL</h5>
+            <p style={styleParagrafo}>Alto impacto visual, baixo impacto ambiental!</p>
           </div>
         </div>
       </div>
