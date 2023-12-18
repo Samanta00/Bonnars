21f1c07e1ccf72ab83bacd7227445489913cf12f
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-18 20:44:40 -0300

    feat: criação de listar produtos pela navbar

--- front/projeto/src/components/NavBar/index.tsx
+++ front/projeto/src/components/NavBar/index.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-
+  interface Product {
+    id: number;
+    nome: string;
+   
+  }
 
 const NavBar: React.FC = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [products, setProducts] = useState<Product[]>([]);
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/produtos')
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+        setFilteredProducts(data);
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar os produtos:', error);
+      });
+  }, []);
+
+  const handleSearch = (e:any) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+
+    const filtered = products.filter((product) =>
+      product.nome.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  };
+
   return (
     <nav className="navbar navbar-expand-md bg-black navbar-dark">
       <div className="container-fluid">
@@ -32,8 +63,33 @@ const NavBar: React.FC = () => {
           <Link to="/carrinho">
             <i className="bi bi-cart4 text-light fs-3 me-2"></i>
           </Link>
-            <input className="form-control me-2 rounded-0" type="search" placeholder="Procurar produto" aria-label="Buscar" />
-            <button className="btn btn-outline-light rounded-0" type="submit">Buscar</button>
+          <div className="position-relative">
+            <input
+              className="form-control me-2 rounded-0"
+              type="search"
+              placeholder="Procurar produto"
+              aria-label="Buscar"
+              value={searchTerm}
+              onChange={handleSearch}
+            />
+
+            {searchTerm && (
+              <div className="position-absolute bg-white w-100 mt-1 p-2 border rounded">
+                <ul className="list-group">
+                  {filteredProducts.map((product) => (
+                  <li key={product.id} className="list-group-item">
+                    <Link to="/catalogo">{product.nome}</Link>
+                  </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+          
+         
+          <button className="btn btn-outline-light rounded-0">Pesquisar</button>
+        
+          
           </form>
         </div>
       </div>
